c1a6ec7e69ec9f355bddffb9185133176b4a22b2
Stevek <vegh@spsejecna.cz>
2026-03-01 16:03:10 +0100

    Add auth endpoints

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using conquerio.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace conquerio.Data;
+
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<PlayerStats> PlayerStats => Set<PlayerStats>();
+    public DbSet<Leaderboard> Leaderboard => Set<Leaderboard>();
+    public DbSet<GameRun> GameRuns => Set<GameRun>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+
+        builder.Entity<PlayerStats>()
+            .HasOne(ps => ps.User)
+            .WithOne(u => u.PlayerStats)
+            .HasForeignKey<PlayerStats>(ps => ps.UserId);
+
+        builder.Entity<Leaderboard>()
+            .HasOne(lb => lb.User)
+            .WithOne(u => u.Leaderboard)
+            .HasForeignKey<Leaderboard>(lb => lb.UserId);
+
+        builder.Entity<GameRun>()
+            .HasOne(gr => gr.User)
+            .WithMany(u => u.GameRuns)
+            .HasForeignKey(gr => gr.UserId);
+    }
+}
---
+++ Models/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace conquerio.Models;
+
+public class AppUser : IdentityUser
+{
+    // Username and Email are inherited from IdentityUser
+    // PasswordHash is managed by Identity
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    // Navigation properties
+    public PlayerStats? PlayerStats { get; set; }
+    public Leaderboard? Leaderboard { get; set; }
+    public ICollection<GameRun> GameRuns { get; set; } = [];
+}
