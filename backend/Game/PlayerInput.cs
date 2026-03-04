4126ce09388a959fe0994fce6fb46edb94435e15
Jan Koubek <koubek.jenda@icloud.com>
2026-03-04 15:46:03 +0100

    modified player input to include ability

--- backend/Game/PlayerInput.cs
+++ backend/Game/PlayerInput.cs
@@ -3,5 +3,6 @@ namespace conquerio.Game;
 public class PlayerInput
 {
     public required string PlayerId { get; set; }
-    public Direction Direction { get; set; }
+    public Direction? Direction { get; set; }
+    public PlayerAbility? Ability { get; set; }
 }
